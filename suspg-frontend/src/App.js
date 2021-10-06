c4e2f465fd1cdff2f46c6b0b3545a057cdc17397
Jamie <jamiehuff737@gmail.com>
2021-10-06 10:37:42 -0400

    added date selectors to front end + formatting

--- suspg-frontend/src/App.js
+++ suspg-frontend/src/App.js
@@ -1,8 +1,10 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import useApplicationData from './hooks/useApplicationData';
 import Checkbox from '@material-ui/core/Checkbox';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 function App() {
   // const { waterBill, gasBill, electricityBill, setWaterBill, setGasBill, setElectrictyBill } = useApplicationData()
@@ -10,6 +12,8 @@ function App() {
   const [waterChecked, setWaterChecked] = React.useState(true);
   const [gasChecked, setGasChecked] = React.useState(true);
   const [electricityChecked, setElectricityChecked] = React.useState(true);
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
 
   const handleChange = (event) => {
     setWaterChecked(event.target.checked);
@@ -17,40 +21,47 @@ function App() {
     setElectricityChecked(event.target.checked);
   };
 
+  const handleDateChange = (event) => {
+    console.log(endDate)
+  }
+
   return (
     <div>
       <h1>Filters</h1>
-      <ul>
-        <li>
-        <div>
+      <div>
           From
-          from: date
-        </div>
-        </li>
-        <li>
-        <div>
+          <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
           To
-          to: date
-        </div>
-        </li>
-      </ul>
+          <DatePicker selected={endDate} onChange={(date) => setEndDate(date)} />
+      </div>
        
       <h2>Utility Type</h2>
-    <Checkbox
-      waterChecked={waterChecked}
-      onChange={handleChange}
-      inputProps={{ 'aria-label': 'controlled' }}
-    />
-        <Checkbox
-      gasChecked={gasChecked}
-      onChange={handleChange}
-      inputProps={{ 'aria-label': 'controlled' }}
-    />
-        <Checkbox
-      electricityChecked={electricityChecked}
-      onChange={handleChange}
-      inputProps={{ 'aria-label': 'controlled' }}
-    />
+        <ul>
+          <ul>
+            <Checkbox
+              waterChecked={waterChecked}
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+            Electricity
+          </ul>
+          <ul>
+            <Checkbox
+              gasChecked={gasChecked}
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+            Water
+          </ul>
+          <ul>
+            <Checkbox
+              electricityChecked={electricityChecked}
+              onChange={handleChange}
+              inputProps={{ 'aria-label': 'controlled' }}
+            />
+            Gas
+          </ul>
+        </ul>
     </div>
   );
 }
