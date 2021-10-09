6d4d28b44f5e6825deb6c7664c2321d61e3ea28a
Jamie <jamiehuff737@gmail.com>
2021-10-09 01:05:14 -0400

    highchart placeholder added, no real datavalues

--- suspg-frontend/src/App.js
+++ suspg-frontend/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, Component } from "react";
 import logo from './logo.svg';
 import './App.css';
 import useApplicationData from './hooks/useApplicationData';
 import Checkbox from '@material-ui/core/Checkbox';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official'
 
 function App() {
-  // const { waterBill, gasBill, electricityBill, setWaterBill, setGasBill, setElectrictyBill } = useApplicationData()
+  // issues: 
+    // 1: This variable where we import our data is causing the webpage to die? im geussing infinite loadding?
+        //const { waterBill, gasBill, electricityBill, setWaterBill, setGasBill, setElectrictyBill } = useApplicationData()'
+    // 2: Figure out how to use the pages state to display nothing a first, 
+        //then display various data depending on what checkbox
 
   const [waterChecked, setWaterChecked] = React.useState(true);
   const [gasChecked, setGasChecked] = React.useState(true);
@@ -29,6 +35,21 @@ function App() {
   // https://www.npmjs.com/package/react-datepicker need to figure this part out
   const handleDateChange = (event) => {
     console.log(endDate)
+  }
+
+  const options = {
+    title: {
+      text: 'Utility Consumption'
+    },
+    series: [{
+        name: 'power',
+        data: [100, 200, 30, 100, 30, 50, 100]
+      }],
+    yAxis: {
+      title: {
+        text: 'Consumption ( k/Wh )'
+      }
+    }
   }
 
   return (
@@ -68,6 +89,14 @@ function App() {
             Gas
           </ul>
         </ul>
+
+    <div>
+      <HighchartsReact 
+      highcharts={Highcharts} 
+      options={options}
+      />
+    </div>
+
     </div>
   );
 }
